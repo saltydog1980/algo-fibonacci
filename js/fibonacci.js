d997eec3017b63bb07e0bce7859b6207fa137aff
saltydog1980 <mikeelambert99@gmail.com>
2022-05-24 17:12:33 -0400

    final code

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = (num) => {
-    
-}
+    let fibArray = [0, 1];
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        for (let i = 2; i <= num; i++) {
+            fibArray.push(fibArray[i-2] + fibArray[i-1])
+        }
+    }
+    return fibArray[num]
+};
 
 module.exports = {fibonacci}
