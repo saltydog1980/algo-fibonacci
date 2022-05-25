e05b1b5d44125a82ac57e81a4960788e7ef88cec
saltydog1980 <mikeelambert99@gmail.com>
2022-05-25 18:04:04 -0400

    finished python exercise

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,10 @@
 def fibonacci(n):
-  pass
-
-
+  fib_list = [0, 1]
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    for x in range(2, n+1):
+      fib_list.append(fib_list[x-2] + fib_list[x-1])
+  return fib_list[n]
